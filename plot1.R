3b5b4bd446893857fd3785ca13f8090ab3588bdb
Bryan Paulsen <bryanpaulsen@comcast.net>
2020-03-11 17:55:03 -0400

    First plotting file

--- plot1.R
+++ plot1.R
@@ -8,3 +8,5 @@ data$Global_active_power <- as.numeric(data$Global_active_power)
 
 # Draw the histogram
 hist(data$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red")
+dev.copy(png, file = "Plot1.png")
+dev.off()
