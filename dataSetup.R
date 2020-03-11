11ae4c2dd7441a684e7fe4b8a580e90261002188
Bryan Paulsen <bryanpaulsen@comcast.net>
2020-03-11 17:54:29 -0400

    Loads the dataset and reformats the date

--- dataSetup.R
+++ dataSetup.R
@@ -6,7 +6,7 @@ library(lubridate)
 
 dataSetup <- function() {
 	# Read in the text file with a header and using ";" as the column seperator
-	data <- read.table("H:/Other_Projects/household_power_consumption.txt", header = TRUE, sep = ";")
+	data <- read.table("../household_power_consumption.txt", header = TRUE, sep = ";")
 
 	# filters the rows to only include the two days
 	data <- filter(data, Date == "2/2/2007" | Date == "2/1/2007")
